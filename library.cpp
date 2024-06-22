d8551d715eefc986f98dc0033ca2ab078d3984b1
Rutuja <rghadge510@gmail.com>
2024-06-22 23:06:28 +0530

    code changes

--- library.cpp
+++ library.cpp
@@ -140,7 +140,8 @@ void calculateFine() {
     cin.ignore();  // Ignore leftover newline character
     cout << "Enter book ISBN to calculate fine: ";
     getline(cin, ISBN);
-    for (const auto& record : borrowHistory) {
+    for (const auto& record : borrowHistory)
+     {
         if (record.bookISBN == ISBN) {
             if (record.returnDate.empty())
              {
